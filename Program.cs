ba5036ba726b4f9804a90f6c932690557cf00097
Eugene-Bocharov <4003498@gmail.com>
2026-03-18 13:15:43 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// -1 No other room, 2 Some other room, 3 No fucking idea
+Rooms Room0 = new Rooms { Name = "Kitchen", Description = "Wasd", North = -1, South = 2, East = -1, West = -1 };
+
+Rooms Room1 = new Rooms { Name = "Small Bedroom", Description = "Wasd", North= 1, South = 2, West = 3, East = 4 };
+Rooms Room2 = new Rooms { Name = "Main Hallway", Description = "Wasd", North = 1, South = 2, West = 3, East = 4 };
+Rooms Room3 = new Rooms { Name = "Foyer", Description = "Wasd", North = 1, South = 2, West = 3, East = 4 };
+Rooms Room4 = new Rooms { Name = "Living Room", Description = "Wasd" , North = 1, South = 2, West = 3, East = 4};
+
+Rooms[] Map = { Room0, Room1, Room2, Room3, Room4 };
+
+class Player
+{
+    string PlayersName;
+    class PlayerPosition
+    {
+
+    }
+
+    class PlayerKey
+    {
+
+    }
+
+}
+
+class GameStart
+{
+
+}
+
+struct Rooms
+{
+    public string Name, Description;
+    public int North, South, East, West;
+}
